b5e4ede4c8c39a7e4f40521c34eb4d2660af0372
Brian Blais <bblais@gmail.com>
2021-04-01 13:19:57 -0400

    install packages update

---
+++ Install Packages.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+get_ipython().system('pip install "git+git://github.com/bblais/sci378" --upgrade')
+
+
+# In[2]:
+
+
+import sci378
+
+
+# In[3]:
+
+
+sci378.get_course_github()
+
+
+# In[3]:
+
+
+get_ipython().system('pip install "git+git://github.com/bblais/pyndamics3" --upgrade')
+
+
+# In[4]:
+
+
+import pyndamics3
+
+
+# In[6]:
+
+
+get_ipython().system('pip install arviz')
+
+
+# In[5]:
+
+
+get_ipython().system('pip install tqdm')
+
+
+# In[ ]:
+
+
+
+
